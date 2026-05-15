3f8c0382f7d63f6e6ab3731f7a9b7e081f93bda2
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2026-05-15 13:31:40 +0900

    Работа с исключениями

--- 13.05.2026/1/obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ 13.05.2026/1/obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93356d22232bf592ff940792ff666714accc5de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 14.05.2026- 1/9/obj/Debug/net10.0/9.AssemblyInfo.cs
+++ 14.05.2026- 1/9/obj/Debug/net10.0/9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93356d22232bf592ff940792ff666714accc5de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
